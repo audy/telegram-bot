70029e0733e17ca7b42f5258546ae9f2c9b1aee4
Austin Davis-Richardson <harekrishna@gmail.com>
2019-02-12 17:39:21 -0800

    pizza

--- bot.py
+++ bot.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
 from telethon import TelegramClient, sync, events
+import time
 
 API_ID = 566798
 API_HASH = "f44dfd753ceef8b2676b3f474cf8cc5f"
 
 client = TelegramClient("session", API_ID, API_HASH).start(phone="+1 415 941 9726")
 
-print(client.get_me().stringify())
-
 @client.on(events.NewMessage(pattern="hi"))
-async def hander(event):
-    print(event)
+async def hi(event):
     await event.respond("bot: hey!")
+
+@client.on(events.NewMessage(pattern="pizza"))
+async def pizza(event):
+    await event.respond("bot: ordering a pizza. please wait...")
+    time.sleep(10)
+    await event.respond("bot: pizza ordered!")
 
 client.run_until_disconnected()
