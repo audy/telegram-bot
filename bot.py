3b4c5dd69be9996861c6f7183d76bb36ec2f7e8c
Austin Davis-Richardson <harekrishna@gmail.com>
2019-02-12 20:10:30 -0800

    more greetings

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 from telethon import TelegramClient, sync, events
+import re
+import random
 import time
 
-API_ID = 566798
-API_HASH = "f44dfd753ceef8b2676b3f474cf8cc5f"
+API_ID = 771357
+API_HASH = "696dba4ddacd457a72fc8317b9f01866"
 
-client = TelegramClient("session", API_ID, API_HASH).start(phone="+1 415 941 9726")
+client = TelegramClient("session", API_ID, API_HASH).start(phone="+1 352 200 2839")
 
-@client.on(events.NewMessage(pattern="hi"))
+
+print("---> running")
+
+
+@client.on(events.NewMessage(pattern=re.compile(r"hi", re.IGNORECASE)))
 async def hi(event):
-    await event.respond("bot: hey!")
+    greetings = ["Hello!", "Sup?", "Hi!", "Hallo", "¡Hola!"]
+    await event.respond(random.choice(greetings))
 
-@client.on(events.NewMessage(pattern="pizza"))
-async def pizza(event):
-    await event.respond("bot: ordering a pizza. please wait...")
-    time.sleep(10)
-    await event.respond("bot: pizza ordered!")
 
 client.run_until_disconnected()
