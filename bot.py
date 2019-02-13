eeba4b46f8ec7ba0dbda649d55dc330c37a830c4
Austin Davis-Richardson <harekrishna@gmail.com>
2019-02-12 20:50:08 -0800

    joke

--- bot.py
+++ bot.py
@@ -16,6 +16,13 @@ CAT_API_KEY = "9f053a85-ef38-470b-914e-d1c3bffafd65"
 client = TelegramClient("session", API_ID, API_HASH).start(phone="+1 352 200 2839")
 
 
+def get_random_joke():
+    resp = requests.get(
+        "https://icanhazdadjoke.com/", headers={"Accept": "application/json"}
+    )
+    return resp.json()["joke"]
+
+
 def get_random_cat():
     resp = requests.get("https://api.thecatapi.com/v1/images/search?size=full")
     return resp.json()[0]["url"]
@@ -27,6 +34,11 @@ print("---> running")
 @client.on(events.NewMessage(pattern=re.compile(r"cat", re.IGNORECASE)))
 async def cat(event):
     await event.respond(get_random_cat())
+
+
+@client.on(events.NewMessage(pattern=re.compile(r"joke", re.IGNORECASE)))
+async def joke(event):
+    await event.respond(get_random_joke())
 
 
 @client.on(events.NewMessage(pattern=re.compile(r"hi", re.IGNORECASE)))
