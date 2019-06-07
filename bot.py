de84c446d782e1fa9e6b3b0bbc14b5dd6ff2429a
Austin Davis-Richardson <harekrishna@gmail.com>
2019-06-06 23:21:48 -0700

    switches to telegram-bot

--- bot.py
+++ bot.py
@@ -1,56 +1,46 @@
 #!/usr/bin/env python3
 
-from telethon import TelegramClient, sync, events
+from telegram.ext import Updater, CommandHandler
+
 import re
 import random
 import time
 import requests
+import logging
 
-API_ID = 771357
-API_HASH = "696dba4ddacd457a72fc8317b9f01866"
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
-# thecatapi.com
-CAT_API_KEY = "9f053a85-ef38-470b-914e-d1c3bffafd65"
+updater = Updater(
+    token="636614158:AAEDn80O9QQyOq5PW1a8lk1Yb2SUhBlny2I", use_context=True
+)
 
 
-client = TelegramClient("session", API_ID, API_HASH).start(phone="+1 352 200 2839")
-
-
-def get_random_joke():
+def joke(update, context):
     resp = requests.get(
         "https://icanhazdadjoke.com/", headers={"Accept": "application/json"}
     )
-    return resp.json()["joke"]
+    joke = resp.json()["joke"]
+    context.bot.send_message(chat_id=update.message.chat_id, text=joke)
 
 
-def get_random_cat():
+def cat(update, context):
     resp = requests.get("https://api.thecatapi.com/v1/images/search?size=full")
-    return resp.json()[0]["url"]
+    cat_url = resp.json()[0]["url"]
+    context.bot.send_message(chat_id=update.message.chat_id, text=cat_url)
 
 
-print("---> running")
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.message.chat_id, text="I'm a bot, please talk to me!"
+    )
 
 
-@client.on(events.NewMessage(pattern=re.compile(r"cat", re.IGNORECASE)))
-async def cat(event):
-    await event.respond(get_random_cat())
+start_handler = CommandHandler("start", start)
+updater.dispatcher.add_handler(start_handler)
 
+cat_handler = CommandHandler("cat", cat)
+updater.dispatcher.add_handler(cat_handler)
 
-@client.on(events.NewMessage(pattern=re.compile(r"joke", re.IGNORECASE)))
-async def joke(event):
-    await event.respond(get_random_joke())
-
-
-@client.on(events.NewMessage(pattern=re.compile(r"hi", re.IGNORECASE)))
-async def hi(event):
-    greetings = ["Hello!", "Sup?", "Hi!", "Hallo", "¡Hola!"]
-    await event.respond(random.choice(greetings))
-
-
-@client.on(events.NewMessage(pattern=re.compile(r"Bye", re.IGNORECASE)))
-async def bye(event):
-    greetings = ["Bye.", "oh wow!", "FINE", "Goodbye", "Love You"]
-    await event.respond(random.choice(greetings))
-
-
-client.run_until_disconnected()
+updater.start_polling()
