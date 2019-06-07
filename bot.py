d62cbb00cd0847bae3c212a3e9b17a1f73187f1f
Austin Davis-Richardson <harekrishna@gmail.com>
2019-06-06 23:30:12 -0700

    makes it a bot

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@
 
 from telegram.ext import Updater, CommandHandler
 
-import re
+from neighborhoods import NEIGHBORHOODS
+
 import random
-import time
 import requests
 import logging
 
@@ -17,7 +17,21 @@ updater = Updater(
 )
 
 
+def cat(update, context):
+    """ a random cat photo """
+    resp = requests.get("https://api.thecatapi.com/v1/images/search?size=full")
+    cat_url = resp.json()[0]["url"]
+    context.bot.send_message(chat_id=update.message.chat_id, text=cat_url)
+
+
+def hood(update, context):
+    """ a random neighborhood in SF """
+    neighborhood = random.choice(NEIGHBORHOODS)
+    context.bot.send_message(chat_id=update.message.chat_id, text=neighborhood)
+
+
 def joke(update, context):
+    """ tell a joke """
     resp = requests.get(
         "https://icanhazdadjoke.com/", headers={"Accept": "application/json"}
     )
@@ -25,22 +39,29 @@ def joke(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=joke)
 
 
-def cat(update, context):
-    resp = requests.get("https://api.thecatapi.com/v1/images/search?size=full")
-    cat_url = resp.json()[0]["url"]
-    context.bot.send_message(chat_id=update.message.chat_id, text=cat_url)
-
-
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.message.chat_id, text="I'm a bot, please talk to me!"
+def weather(update, context):
+    resp = requests.get(
+        "https://api.darksky.net/forecast/d3e344b04f2da052a8b96431bf58131d/37.8267,-122.4233"
     )
 
+    print(resp)
 
-start_handler = CommandHandler("start", start)
-updater.dispatcher.add_handler(start_handler)
+    weather_data = resp.json()
 
-cat_handler = CommandHandler("cat", cat)
-updater.dispatcher.add_handler(cat_handler)
+    weather_summary = f"""
+Currently: {weather_data['minutely']['summary']}
+(Wind={weather_data['currently']['windSpeed']}-mph; Temp={weather_data['currently']['temperature']}-F Humidity={100*weather_data['currently']['humidity']}%)
+Today: {weather_data['hourly']['summary']}
+Forecast: {weather_data['daily']['summary']}
+"""
 
+    context.bot.send_message(chat_id=update.message.chat_id, text=weather_summary)
+
+
+commands = {"cat": cat, "hood": hood, "hoke": joke, "weather": weather}
+
+for command_name, command_function in commands.items():
+    updater.dispatcher.add_handler(CommandHandler("command_name", command_function))
+
+print("listening")
 updater.start_polling()
