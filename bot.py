e848c5b0a8189de1bff93c2b971570ed2694f40f
Austin Davis-Richardson <harekrishna@gmail.com>
2019-10-25 18:08:33 -0700

    bored

--- bot.py
+++ bot.py
@@ -7,14 +7,42 @@ from neighborhoods import NEIGHBORHOODS
 import random
 import requests
 import logging
+from yelpapi import YelpAPI
+
+YELP_API_KEY = "q1CJG9aPlwTzWx_l-O6eEJ_YE9Dnz4Ej1Y8iN3BT5xxsa_qetRrxqhNQxDLVjcE_R6V-oSWFRoRk_C-u8HZxxmq1ZeXISygpgo2I-e675dn-cymOrRmjforBp5KzXXYx"
+yelp = YelpAPI(YELP_API_KEY)
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
 updater = Updater(
     token="636614158:AAEDn80O9QQyOq5PW1a8lk1Yb2SUhBlny2I", use_context=True
 )
+
+
+def bored():
+    first_neighborhood = random.choice(NEIGHBORHOODS)
+
+    restaurants = yelp.search_query(
+        location=neighborhood, limit=10, open_now=True, categories="restaurants"
+    )
+
+    restaurant = random.sample(restaurants["businesses"], 1)[0]
+
+    second_neighborhood = random.choice(NEIGHBORHOODS)
+
+    bars = yelp.search_query(
+        location=neighborhood, limit=10, open_now=True, categories="bars"
+    )
+
+    message = (
+        f"First, take a trip to {first_neighborhood} and get a bite to eat at {restaurant['name']} {(', '.join(c['title'] for c in restaurant['categories']))}"
+        f"Then, head to {second_neighborhood} and get a drink at {bar}"
+    )
+
+    context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
 def cat(update, context):
@@ -53,13 +81,95 @@ Today: {weather_data['hourly']['summary']}
 Forecast: {weather_data['daily']['summary']}
 """
 
-    context.bot.send_message(chat_id=update.message.chat_id, text=weather_summary)
+    context.bot.send_message(
+        chat_id=update.message.chat_id, text=weather_summary
+    )
 
 
-commands = {"cat": cat, "hood": hood, "joke": joke, "weather": weather}
+def _humanized_list(items):
+    if len(items) == 1:
+        return items[0]
+    else:
+        return ", ".join(items[:1]) + f" & {items[-1]}"
+
+
+def _get_movement_action():
+    return random.choice(
+        [
+            "take a trip to",
+            "head on over to",
+            "walk on down to",
+            "make a quick jaunt to",
+            "find yourself in",
+            "ride the bus to",
+            "have a nice bike ride over to",
+        ]
+    )
+
+
+def bored(update, context):
+    first_neighborhood = random.choice(NEIGHBORHOODS)
+
+    restaurant = random.choice(
+        yelp.search_query(
+            location=first_neighborhood,
+            limit=10,
+            open_now=True,
+            categories="restaurants",
+        )["businesses"]
+    )
+
+    restaurant_categories = [
+        category["title"] for category in restaurant["categories"]
+    ]
+
+    second_neighborhood = random.choice(NEIGHBORHOODS)
+
+    bar = random.choice(
+        yelp.search_query(
+            location=second_neighborhood,
+            limit=10,
+            open_now=True,
+            categories="bars",
+        )["businesses"]
+    )
+
+    eat_action = random.choice(
+        ["grab a bite", "have a snack", "get some grub", "enjoy the nice food"]
+    )
+
+    drink_action = random.choice(
+        [
+            "grab a drink",
+            "smash a few whiteclaws",
+            "have a cold one",
+            "take it easy",
+        ]
+    )
+
+    message = " ".join(
+        [
+            f"First, {_get_movement_action()} {first_neighborhood} and",
+            f"{eat_action} at {restaurant['name']} ({_humanized_list(restaurant_categories)}).",
+            f"Then, {_get_movement_action()} {second_neighborhood} and {drink_action} at {bar['name']}.",
+        ]
+    )
+
+    context.bot.send_message(chat_id=update.message.chat_id, text=message)
+
+
+commands = {
+    "cat": cat,
+    "hood": hood,
+    "joke": joke,
+    "weather": weather,
+    "bored": bored,
+}
 
 for command_name, command_function in commands.items():
-    updater.dispatcher.add_handler(CommandHandler(command_name, command_function))
+    updater.dispatcher.add_handler(
+        CommandHandler(command_name, command_function)
+    )
 
 print("listening")
 updater.start_polling()
