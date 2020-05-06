cd8463a9a95128f1046c1cc4719133725407dd92
Austin Davis-Richardson <harekrishna@gmail.com>
2020-05-06 00:39:27 -0700

    rm debug print

--- test_bot.py
+++ test_bot.py
@@ -5,7 +5,6 @@ import os
 
 @pytest.fixture(scope="module")
 def keys():
-    print("setting keys!")
     os.environ["YELP_API_KEY"] = "yelp-api-key"
     os.environ["TELEGRAM_API_KEY"] = "telegram-api-key"
     os.environ["DARKSKY_API_KEY"] = "darksky-api-key"
